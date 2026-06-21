b81ebcb1ae7462c8c81dee6f359b4773f45bb315
sea-deep <seadeepie@gmail.com>
2026-06-21 21:44:02 +0530

    feat(idle): linearly scale timeout limits to permit a 3-minute screen dim threshold

--- sway/idle.sh
+++ sway/idle.sh
@@ -3,8 +3,8 @@
 pkill -x swayidle
 
 swayidle -w \
-         timeout 60 'brightnessctl -s set 10' resume 'brightnessctl -r' \
-         timeout 120 'swaylock -f -i /home/dipak/code/arch-theme/wallpapers/satisfaction_hires_lock_final.png' \
-         timeout 300 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
-         timeout 600 'systemctl suspend' \
+         timeout 180 'brightnessctl -s set 10' resume 'brightnessctl -r' \
+         timeout 300 'swaylock -f -i /home/dipak/code/arch-theme/wallpapers/satisfaction_hires_lock_final.png' \
+         timeout 600 'swaymsg "output * power off"' resume 'swaymsg "output * power on"' \
+         timeout 900 'systemctl suspend' \
          before-sleep 'swaylock -f -i /home/dipak/code/arch-theme/wallpapers/satisfaction_hires_lock_final.png'

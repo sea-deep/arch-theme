0bacbb2c7f6147702ea24ea1150b76a0303a73ec
sea-deep <seadeepie@gmail.com>
2026-07-02 01:48:27 +0530

    fix: clipse kitty margins and border color

--- sway/clipse-toggle.sh
+++ sway/clipse-toggle.sh
@@ -2,5 +2,5 @@
 if swaymsg -t get_tree | grep -q '"app_id": "clipse"'; then
     swaymsg '[app_id="clipse"] kill'
 else
-    kitty --class clipse -e clipse
+    kitty --class clipse -o window_margin_width=4 -o window_border_width=2 -o draw_minimal_borders=no -o active_border_color="#39c5bb" -o inactive_border_color="#39c5bb" -e clipse
 fi

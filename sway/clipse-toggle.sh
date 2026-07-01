98845a78ce1dc71b1a99bad7f87e10f7da8fb5cc
sea-deep <seadeepie@gmail.com>
2026-07-02 01:54:19 +0530

    fix: re-enable window decorations on clipse to restore swayfx borders

--- sway/clipse-toggle.sh
+++ sway/clipse-toggle.sh
@@ -2,5 +2,5 @@
 if swaymsg -t get_tree | grep -q '"app_id": "clipse"'; then
     swaymsg '[app_id="clipse"] kill'
 else
-    kitty --class clipse -o window_margin_width=4 -e clipse
+    kitty --class clipse -o hide_window_decorations=no -o window_margin_width=4 -e clipse
 fi

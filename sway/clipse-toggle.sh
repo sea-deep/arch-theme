962a6313ba706c7ae4a92c0af2323b326e18bc4c
sea-deep <seadeepie@gmail.com>
2026-07-02 01:52:32 +0530

    fix: remove hardcoded square kitty border to allow swayfx rounded borders

--- sway/clipse-toggle.sh
+++ sway/clipse-toggle.sh
@@ -2,5 +2,5 @@
 if swaymsg -t get_tree | grep -q '"app_id": "clipse"'; then
     swaymsg '[app_id="clipse"] kill'
 else
-    kitty --class clipse -o window_margin_width=4 -o window_border_width=2 -o draw_window_borders_for_single_window=yes -o active_border_color="#39c5bb" -o inactive_border_color="#39c5bb" -e clipse
+    kitty --class clipse -o window_margin_width=4 -e clipse
 fi
